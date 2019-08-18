1d496ab5a3d548cf0477b890ec3a7d4ff02b78a9
Nicolas A Perez <anicolaspp@gmail.com>
2019-08-18 10:28:43 -0400

    Update MyJournalSpec.scala

--- src/test/scala/MyJournalSpec.scala
+++ src/test/scala/MyJournalSpec.scala
@@ -6,23 +6,9 @@ import akka.persistence.journal.JournalSpec
 import com.typesafe.config.ConfigFactory
 
 class MyJournalSpec extends JournalSpec(config = ConfigFactory.parseString("""
-    akka.persistence.journal.plugin = "my.journal.plugin"
+    akka.persistence.journal.plugin = "akka-persistence-maprdb"
     """)) {
 
-  override def supportsRejectingNonSerializableObjects: CapabilityFlag =
-    true // or CapabilityFlag.on
-
-//  val storageLocations = List(
-//    new File(system.settings.config.getString("akka.persistence.journal.leveldb.dir")),
-//    new File(config.getString("akka.persistence.snapshot-store.local.dir")))
-
-  override def beforeAll(): Unit = {
-//    super.beforeAll()
-//    storageLocations.foreach(FileUtils.deleteRecursively)
-  }
-
-  override def afterAll(): Unit = {
-
-  }
+  override def supportsRejectingNonSerializableObjects: CapabilityFlag = false // or CapabilityFlag.on
 
 }
