97b6cba1eec6bc178889ae461c0002bd5e635b53
Nicolas A Perez <anicolaspp@gmail.com>
2019-08-19 14:03:53 -0400

    creating source objects

--- src/main/scala/com/github/anicolaspp/akka/persistence/query/MapRDBScalaReadJournal.scala
+++ src/main/scala/com/github/anicolaspp/akka/persistence/query/MapRDBScalaReadJournal.scala
@@ -4,16 +4,10 @@ import akka.NotUsed
 import akka.actor.ExtendedActorSystem
 import akka.persistence.query.scaladsl.{CurrentPersistenceIdsQuery, ReadJournal}
 import akka.stream.scaladsl.Source
-import akka.stream.stage._
-import akka.stream.{Attributes, Outlet, SourceShape}
 import com.github.anicolaspp.akka.persistence.ojai.StorePool
+import com.github.anicolaspp.akka.persistence.query.sources.CurrentPersistenceIdsSource
 import com.github.anicolaspp.akka.persistence.{MapRDB, MapRDBConnectionProvider}
 import com.typesafe.config.Config
-import org.ojai.store.{Connection, DocumentStore}
-
-import scala.collection.mutable
-import scala.concurrent.ExecutionContextExecutor
-import scala.util.Try
 
 class MapRDBScalaReadJournal(system: ExtendedActorSystem, config: Config) extends ReadJournal
   with CurrentPersistenceIdsQuery
@@ -34,65 +28,5 @@ object MapRDBScalaReadJournal {
   def apply(system: ExtendedActorSystem, config: Config): MapRDBScalaReadJournal = new MapRDBScalaReadJournal(system: ExtendedActorSystem, config: Config)
 }
 
-class CurrentPersistenceIdsSource(store: DocumentStore)(implicit connection: Connection)
-  extends GraphStage[SourceShape[String]] {
 
-  private val out: Outlet[String] = Outlet("CurrentPersistenceIdsSource")
 
-  override def shape: SourceShape[String] = SourceShape(out)
-
-  override def createLogic(inheritedAttributes: Attributes): GraphStageLogic = new GraphStageLogicWithLogging(shape) {
-    private var start = true
-    private var index = 0
-    private var buffer = mutable.Queue.empty[String]
-
-    private implicit def ec: ExecutionContextExecutor = materializer.executionContext
-
-    //    private val StringSeq = classTag[Seq[String]]
-
-    setHandler(out, new OutHandler {
-      override def onPull(): Unit = {
-        if (buffer.isEmpty && (start || index > 0)) {
-          val callback = getAsyncCallback[Seq[String]] { docs =>
-
-            start = false
-            buffer.enqueue(docs: _*)
-
-            deliver()
-          }
-
-          tryQuery(store).fold(
-            e => getAsyncCallback[Unit] { _ => failStage(e) }.invoke(()),
-            value => callback.invoke(value))
-
-        } else {
-          deliver()
-        }
-      }
-    }
-
-    )
-
-    private def tryQuery(store: DocumentStore) = Try {
-      import scala.collection.JavaConverters._
-
-      store.find().asScala.map(_.getIdString).toSeq
-    }
-
-    private def deliver(): Unit = {
-      if (buffer.nonEmpty) {
-        val elem = buffer.dequeue
-        push(out, elem)
-      } else {
-        // we're done here, goodbye
-        completeStage()
-      }
-    }
-  }
-}
-
-object CurrentPersistenceIdsSource {
-
-  case object Continue
-
-}
---
+++ src/main/scala/com/github/anicolaspp/akka/persistence/query/sources/CurrentPersistenceIdsSource.scala
@@ -0,0 +1,66 @@
+package com.github.anicolaspp.akka.persistence.query.sources
+
+import akka.stream.stage.{GraphStage, GraphStageLogic, GraphStageLogicWithLogging, OutHandler}
+import akka.stream.{Attributes, Outlet, SourceShape}
+import org.ojai.store.{Connection, DocumentStore}
+
+import scala.collection.mutable
+import scala.concurrent.ExecutionContextExecutor
+import scala.util.Try
+
+class CurrentPersistenceIdsSource(store: DocumentStore)(implicit connection: Connection)
+  extends GraphStage[SourceShape[String]] {
+
+  private val out: Outlet[String] = Outlet("CurrentPersistenceIdsSource")
+
+  override def shape: SourceShape[String] = SourceShape(out)
+
+  override def createLogic(inheritedAttributes: Attributes): GraphStageLogic = new GraphStageLogicWithLogging(shape) {
+    private var start = true
+    private val index = 0
+    private val buffer = mutable.Queue.empty[String]
+
+    private implicit def ec: ExecutionContextExecutor = materializer.executionContext
+
+    setHandler(out, new OutHandler {
+      override def onPull(): Unit = {
+        if (buffer.isEmpty && (start || index > 0)) {
+          val callback = getAsyncCallback[Seq[String]] { docs =>
+
+            start = false
+            buffer.enqueue(docs: _*)
+
+            deliver()
+          }
+
+          tryQuery(store).fold(
+            e => getAsyncCallback[Unit] { _ => failStage(e) }.invoke(()),
+            value => callback.invoke(value))
+
+        } else {
+          deliver()
+        }
+      }
+    })
+
+    private def tryQuery(store: DocumentStore) = Try {
+      import scala.collection.JavaConverters._
+
+      store.find().asScala.map(_.getIdString).toSeq
+    }
+
+    private def deliver(): Unit = {
+      if (buffer.nonEmpty) {
+        val elem = buffer.dequeue
+        push(out, elem)
+      } else {
+        // we're done here, goodbye
+        completeStage()
+      }
+    }
+  }
+}
+
+object CurrentPersistenceIdsSource {
+  case object Continue
+}
