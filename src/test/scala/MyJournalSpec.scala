2385613bad1a89d9eaaae3cd6c98a06a4e6884c7
Nicolas A Perez <anicolaspp@gmail.com>
2019-08-18 22:19:18 -0400

    Update MyJournalSpec.scala

--- src/test/scala/MyJournalSpec.scala
+++ src/test/scala/MyJournalSpec.scala
@@ -1,14 +1,14 @@
-package com.github.anicolaspp.akka.persistence
-package test
-
-import akka.persistence.CapabilityFlag
-import akka.persistence.journal.JournalSpec
-import com.typesafe.config.ConfigFactory
-
-//class MyJournalSpec extends JournalSpec(config = ConfigFactory.parseString("""
-//    akka.persistence.journal.plugin = "akka-persistence-maprdb"
-//    """)) {
+//package com.github.anicolaspp.akka.persistence
+//package test
 //
-//  override def supportsRejectingNonSerializableObjects: CapabilityFlag = false // or CapabilityFlag.on
+//import akka.persistence.CapabilityFlag
+//import akka.persistence.journal.JournalSpec
+//import com.typesafe.config.ConfigFactory
 //
-}
+////class MyJournalSpec extends JournalSpec(config = ConfigFactory.parseString("""
+////    akka.persistence.journal.plugin = "akka-persistence-maprdb"
+////    """)) {
+////
+////  override def supportsRejectingNonSerializableObjects: CapabilityFlag = false // or CapabilityFlag.on
+////
+//}
