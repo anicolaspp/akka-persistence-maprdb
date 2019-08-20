9b3cf7c5720425e5c1f284de902fbadbeec9283d
Nicolas A Perez <anicolaspp@gmail.com>
2019-08-19 23:15:31 -0400

    Update MyJournalSpec.scala

--- src/test/scala/MyJournalSpec.scala
+++ src/test/scala/MyJournalSpec.scala
@@ -7,22 +7,22 @@ import com.github.anicolaspp.akka.persistence.MapRDBConnectionProvider
 import com.typesafe.config.{Config, ConfigFactory}
 import org.scalatest.Ignore
 
-@Ignore
-class MyJournalSpec extends JournalSpec(config = ConfigFactory.parseString(
-  """
-    akka.persistence.journal.plugin = "akka-persistence-maprdb.journal"
-    """)) with MapRDBConnectionProvider {
-
-  override def supportsRejectingNonSerializableObjects: CapabilityFlag = false // or CapabilityFlag.on
-
-  override def actorSystemConfiguration: Config = system.settings.config
-
-  override def preparePersistenceId(pid: String): Unit = {
-    val storePath = actorSystemConfiguration.getString(MapRDB.PATH_CONFIGURATION_KEY) + s"/$pid.journal"
-
-    if (connection.storeExists(storePath)) {
-      connection.deleteStore(storePath)
-    }
-
-  }
-}
+//@Ignore
+//class MyJournalSpec extends JournalSpec(config = ConfigFactory.parseString(
+//  """
+//    akka.persistence.journal.plugin = "akka-persistence-maprdb.journal"
+//    """)) with MapRDBConnectionProvider {
+//
+//  override def supportsRejectingNonSerializableObjects: CapabilityFlag = false // or CapabilityFlag.on
+//
+//  override def actorSystemConfiguration: Config = system.settings.config
+//
+//  override def preparePersistenceId(pid: String): Unit = {
+//    val storePath = actorSystemConfiguration.getString(MapRDB.PATH_CONFIGURATION_KEY) + s"/$pid.journal"
+//
+//    if (connection.storeExists(storePath)) {
+//      connection.deleteStore(storePath)
+//    }
+//
+//  }
+//}
