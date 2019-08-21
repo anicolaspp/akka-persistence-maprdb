35081db4d960b1f64160216d20a97cd16d4241d1
Nicolas A Perez <anicolaspp@gmail.com>
2019-08-21 16:01:40 -0400

    Tagged Events

--- src/main/scala/com/github/anicolaspp/akka/persistence/examples/SnapshotExample.scala
+++ src/main/scala/com/github/anicolaspp/akka/persistence/examples/SnapshotExample.scala
@@ -1,35 +1,43 @@
 package com.github.anicolaspp.akka.persistence.examples
 
 import akka.actor.{ActorSystem, Props}
+import akka.persistence.journal.Tagged
 import akka.persistence.{PersistentActor, SaveSnapshotFailure, SaveSnapshotSuccess, SnapshotOffer}
 
 object SnapshotExample extends App {
+
   final case class ExampleState(received: List[String] = Nil) {
     def updated(s: String): ExampleState = copy(s :: received)
+
     override def toString = received.reverse.toString
   }
 
   class ExamplePersistentActor extends PersistentActor {
-    def persistenceId: String = "sample-id-3"
+    override def persistenceId: String = "sample-id-3"
 
     var state = ExampleState()
 
-    def receiveCommand: Receive = {
-      case "print"                               => println("current state = " + state)
-      case "snap"                                => saveSnapshot(state)
-      case SaveSnapshotSuccess(metadata)         => // ...
+    override def receiveCommand: Receive = {
+      case "print" => println("current state = " + state)
+      case "snap" => saveSnapshot(state)
+      case SaveSnapshotSuccess(metadata) => // ...
       case SaveSnapshotFailure(metadata, reason) => // ...
       case s: String =>
         persist(s) { evt => state = state.updated(evt) }
+
+      case (s: String, tags: Seq[String]) =>
+        persist(Tagged(s, tags.toSet)) { evt => state = state.updated(evt.payload.toString) }
     }
 
-    def receiveRecover: Receive = {
+    override def receiveRecover: Receive = {
       case SnapshotOffer(_, s: ExampleState) =>
         println("offered state = " + s)
         state = s
       case evt: String =>
         state = state.updated(evt)
     }
+
+//    override def journalPluginId: String = "akka-persistence-maprdb"
   }
 
   val system = ActorSystem("example")
@@ -41,9 +49,10 @@ object SnapshotExample extends App {
   persistentActor ! "snap"
   persistentActor ! "c"
   persistentActor ! "d"
+  persistentActor ! ("nick", Seq("boy", "30"))
   persistentActor ! "print"
 
-  Thread.sleep(1000*60*60)
+  Thread.sleep(1000 * 60 * 60)
   system.terminate()
 }
 
